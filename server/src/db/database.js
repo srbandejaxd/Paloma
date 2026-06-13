02d04c090323005eda288ea42be06b4655f14298
kevinmax30 <kevinalexander2013@proton.me>
2026-06-13 08:52:28 -0500

    woodpeckerv2-nuevos puzzles

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -93,54 +93,336 @@ async function initDb() {
 }
 
 const PUZZLES_BLOCK_1 = [
-  { fen: "r6r/1pp3k1/1b6/p2P1p2/P1N1pn2/2P2PP1/BP5P/4RR1K b - - 0 1", solution: ["Rxh2+", "Kxh2", "Rh8#"] },
-  { fen: "rnb3kr/ppp4p/3b3B/3Pp2n/2BP4/3K1Rp1/PPP3q1/RN1Q4 w - - 0 1", solution: ["Rf8+", "Bxf8", "d6+", "Qd5", "Bxd5+", "Be6", "Bxe6#"] },
-  { fen: "r2q1rk1/pppb1ppp/3b4/4p1P1/4Pn2/2N1B2P/PPPQBP2/2KR3R w - - 0 1", solution: ["Bxf4", "exf4", "e5", "Bxe5", "Qxd7"] },
-  { fen: "2kr4/1pp4p/1p1r4/5Pp1/1P2q3/2P1R2P/P3KP2/1Q1R4 b - - 0 1", solution: ["Rd2+", "Rxd2", "Rxd2+", "Kxd2", "Qxb1"] },
-  { fen: "rn1qk2r/ppp2ppp/5n2/2b1p3/2B1P1b1/3P1N2/PPP3PP/RNBQK2R w KQkq - 0 1", solution: ["Bxf7+", "Kxf7", "Nxe5+", "Kg8", "Nxg4", "h5", "Nxf6+", "Qxf6", "Qf3"] },
-  { fen: "r2k3r/pp1b3p/1qn1p1p1/1B1pPn2/Q7/P4N2/1P1BNPPP/2R3K1 w - - 0 1", solution: ["Rxc6", "bxc6", "Ba5", "Kc7", "Ba6", "c5", "Bb5", "Kb8", "Bxb6", "axb6", "Qb3"] },
-  { fen: "2r1k2r/1b1p2q1/p4p2/4p3/PpB1Pp1p/7P/1PPRQPP1/4R1K1 b k - 0 1", solution: ["f3", "Qxf3", "Rxc4"] },
-  { fen: "r3k2r/p1ppbppp/1pn1q3/4P3/2BP2n1/5NB1/1PP1Q1PP/R4K1R b kq - 0 1", solution: ["Qxc4", "Qxc4", "Ne3+", "Ke2", "Nxc4"] },
-  { fen: "1b6/3n1p2/r1k1p1pp/Pr2P3/1PK2P2/3R4/3B2PP/R7 w - - 0 1", solution: ["Rxd7", "Kxd7", "Kxb5"] },
-  { fen: "2b5/4Q1pp/pp3n1k/3p3q/P2P1P2/BP1B2P1/7P/6K1 w - - 0 1", solution: ["Qxf6+", "Qg6", "Qh4+", "Qh5", "Qxh5+", "Kxh5"] },
-  { fen: "4r2k/1b3Q1p/p1q3p1/1p4B1/2pb4/8/PPB3PP/5R1K w - - 0 1", solution: ["Be4", "Qxe4", "Bf6+", "Bxf6", "Qxf6+", "Kg8", "Qf7+", "Kh8", "Qf6+", "Kg8", "Qf7+", "Kh8", "Qf6+", "Kg8"] },
-  { fen: "r1n5/pp2q1kp/2ppr1p1/4p1Q1/8/2N4R/PPP3PP/5RK1 w - - 0 1", solution: ["Qh6+", "Kh8", "Rf8+", "Qxf8", "Qxf8#"] },
-  { fen: "4rrk1/ppp3pp/3p2n1/3PpqB1/nPP5/6P1/P1NBQP1P/2R1NRK1 b - - 0 1", solution: ["Nc3", "Bxc3", "Bxc1"] },
-  { fen: "2kr3r/p4pp1/2p4p/4p3/2n4q/1NPPnP1P/PP2Q2P/R1K2B1R b - - 0 1", solution: ["Rxd3", "Kb1", "Rhd8", "a3", "Rd2", "Nxd2", "Rxd2", "Qxd2", "Nxd2+"] },
-  { fen: "6k1/5pp1/p1n1r2p/2NQ4/1P1p4/P6P/1B1bqPP1/5RK1 b - - 0 1", solution: ["Qxf1+", "Kh2", "Bf4+", "g3", "Qxf2+", "Qg2", "Bxg3+", "Kh1", "Re1+", "Qf1", "Rxf1#"] },
-  { fen: "r1bqk1nr/pppp3p/2n2p2/b5p1/2BPPp1P/2P2N2/P5P1/RNBQK2R w KQkq - 0 1", solution: ["Nxg5", "Qe7", "Qh5+", "Kf8"] },
-  { fen: "2kr1bnr/p1ppqp1p/bpn5/1N4p1/P2PPp2/5N2/1PP2KPP/R1BQ1B1R w - - 0 1", solution: ["Nxa7+", "Kb7", "a5", "Bxf1", "axb6", "Ba6", "bxc7", "Ra8", "c8=Q+", "Rxc8", "Nxc8"] },
-  { fen: "rn1qk1nr/ppp2ppp/8/2b1p3/2B1P1b1/5N2/PPPP2PP/RNBQK2R w KQkq - 0 1", solution: ["Bxf7+", "Kf8", "Bb3", "Nf6", "h3", "Bxf3", "Qxf3"] },
-  { fen: "1k2r3/2p3p1/p4p2/1p3q1p/1n6/PQ2P3/1P2B2P/2KR4 b - - 0 1", solution: ["Rxe3", "Rd8+", "Kb7", "Qd1", "Qe4", "Kd2", "Nc6", "Rd3", "Rxd3+", "Bxd3", "Qf4+"] },
-  { fen: 'r3kb1r/ppp2ppp/2n5/3qp3/3P4/2N5/PPP2PPP/R2QKB1R w KQkq - 0 1', solution: ['dxe5', 'Qxd1+', 'Kxd1', 'Nxe5', 'f4', 'Ng4'] },
+  {
+    fen: "r6r/1pp3k1/1b6/p2P1p2/P1N1pn2/2P2PP1/BP5P/4RR1K b - - 0 1",
+    solution: ["Rxh2+", "Kxh2", "Rh8#"]
+  },
+  {
+    fen: "rnb3kr/ppp4p/3b3B/3Pp2n/2BP4/3K1Rp1/PPP3q1/RN1Q4 w - - 0 1",
+    solution: ["Rf8+", "Bxf8", "d6+", "Be6", "Bxe6#"]
+  },
+  {
+    fen: "r2q1rk1/pppb1ppp/3b4/4p1P1/4Pn2/2N1B2P/PPPQBP2/2KR3R w - - 0 1",
+    solution: ["Bxf4", "exf4", "e5"]
+  },
+  {
+    fen: "2kr4/1pp4p/1p1r4/5Pp1/1P2q3/2P1R2P/P3KP2/1Q1R4 b - - 0 1",
+    solution: ["Rd2+"]
+  },
+  {
+    fen: "rn1qk2r/ppp2ppp/5n2/2b1p3/2B1P1b1/3P1N2/PPP3PP/RNBQK2R w - - 0 1",
+    solution: ["Bxf7+", "Kxf7", "Nxe5+"]
+  },
+  {
+    fen: "r2k3r/pp1b3p/1qn1p1p1/1B1pPn2/Q7/P4N2/1P1BNPPP/2R3K1 w - - 0 1",
+    solution: ["Rxc6", "bxc6", "Ba5"]
+  },
+  {
+    fen: "2r1k2r/1b1p2q1/p4p2/4p3/PpB1Pp1p/7P/1PPRQPP1/4R1K1 b - - 0 1",
+    solution: ["f3", "Qxf3", "Rxc4"]
+  },
+  {
+    fen: "r3k2r/p1ppbppp/1pn1q3/4P3/2BP2n1/5NB1/1PP1Q1PP/R4K1R b - - 0 1",
+    solution: ["Qxc4", "Qxc4", "Ne3+", "Ke2", "Nxc4"]
+  },
+  {
+    fen: "1b6/3n1p2/r1k1p1pp/Pr2P3/1PK2P2/3R4/3B2PP/R7 w - - 0 1",
+    solution: ["Rxd7", "Kxd7", "Kxb5"]
+  },
+  {
+    fen: "2b5/4Q1pp/pp3n1k/3p3q/P2P1P2/BP1B2P1/7P/6K1 w - - 0 1",
+    solution: ["Qxf6+", "gxf6", "Bf8#"]
+  },
+  {
+    fen: "4r2k/1b3Q1p/p1q3p1/1p4B1/2pb4/8/PPB3PP/5R1K w - - 0 1",
+    solution: ["Be4", "Qxe4", "Bf6+", "Bxf6", "Qxf6+"]
+  },
+  {
+    fen: "r1n5/pp2q1kp/2ppr1p1/4p1Q1/8/2N4R/PPP3PP/5RK1 w - - 0 1",
+    solution: ["Qh6+", "Kg8", "Rf8+", "Qxf8", "Qxh7#"]
+  },
+  {
+    fen: "4rrk1/ppp3pp/3p2n1/3Ppqb1/nPP5/6P1/P1NBQP1P/2R1NRK1 b - - 0 1",
+    solution: ["Nc3", "Bxc3", "Bxc1"]
+  },
+  {
+    fen: "2kr3r/p4pp1/2p4p/4p3/2n4q/1NPPnP1P/PP2Q2P/R1K2B1R b - - 0 1",
+    solution: ["Rxd3", "Bg2", "Rhd8", "a4", "Rd1+", "Rxd1", "Rxd1+", "Qxd1", "Nxd1"]
+  },
+  {
+    fen: "6k1/5pp1/p1n1r2p/2NQ4/1P1p4/P6P/1B1bqPP1/5RK1 b - - 0 1",
+    solution: ["Qxf1+", "Kxf1", "Re1#"]
+  },
+  {
+    fen: "r1bqk1nr/pppp3p/2n2p2/b5p1/2BPPp1P/2P2N2/P5P1/RNBQK2R w - - 0 1",
+    solution: ["Nxg5", "fxg5", "Qh5+", "Ke7", "Qf7+", "Kd6", "e5+", "Nxe5", "dxe5+", "Kxe5", "Qd5+", "Kf6", "Qxg5#"]
+  },
+  {
+    fen: "2kr1bnr/p1ppqp1p/bpn5/1N4p1/P2PPp2/5N2/1PP2KPP/R1BQ1B1R w - - 0 1",
+    solution: ["Nxa7+", "Nxa7", "Bxa6+"]
+  },
+  {
+    fen: "rn1qk1nr/ppp2ppp/8/2b1p3/2B1P1b1/5N2/PPPP2PP/RNBQK2R w - - 0 1",
+    solution: ["Bxf7+", "Kxf7", "Nxe5+"]
+  },
+  {
+    fen: "1k2r3/2p3p1/p4p2/1p3q1p/1n6/PQ2P3/1P2B2P/2KR4 b - - 0 1",
+    solution: ["Rxe3", "Qxb4", "Rxe2"]
+  },
+  {
+    fen: "r1bqkbnr/pppp3p/2n2p2/6p1/2BPPp2/5N2/PPP3PP/RNBQK2R w - - 0 1",
+    solution: ["Nxg5", "fxg5", "Qh5+", "Ke7", "Qf7+", "Kd6", "e5+", "Nxe5", "Qd5+", "Ke7", "Qxe5#"]
+  }
 ]
+
 
 const PUZZLES_BLOCK_2 = [
-  { fen: 'r4rk1/ppp2ppp/2n5/3qp3/3P4/2N5/PPP2PPP/R2QKB1R w KQ - 0 1', solution: ['dxe5', 'Qxd1+', 'Rxd1', 'Nxe5', 'Be2'] },
-  { fen: '2rr2k1/pp3pp1/2n4p/3np3/8/1BN1P3/PP3PPP/R3K2R w KQ - 0 1', solution: ['Nxd5', 'Rxd5', 'Bxc6', 'bxc6', 'Rxd5', 'cxd5'] },
-  { fen: 'r2qr1k1/pp3ppp/2p2n2/3p1b2/3P4/2NB1N2/PPP2PPP/R2QR1K1 w - - 0 1', solution: ['Nxd5', 'cxd5', 'Bxf5', 'Qxd4', 'Bg6', 'hxg6', 'Qxd4'] },
-  { fen: '6k1/5p2/6pp/8/8/6PP/5P2/6K1 w - - 0 1', solution: ['f4', 'f5', 'gxf5', 'gxf5', 'h4'] },
-  { fen: 'r1bq1rk1/ppp1nppp/3p1n2/3Pp1B1/2P5/2N5/PP3PPP/R2QKBNR w KQ - 0 8', solution: ['Bxf6', 'Bxf6', 'Nxe5', 'Bxe5', 'Qh5', 'g6', 'Qxe5'] },
-  { fen: '8/5pk1/6p1/7p/8/7P/5PP1/5K2 w - - 0 1', solution: ['g4', 'hxg4', 'hxg4', 'g5', 'f3', 'gxf3', 'Kf2'] },
-  { fen: 'r1bqr1k1/pp3ppp/2n2n2/2pp4/3P4/P1N1PN2/1P3PPP/R1BQKB1R w KQ - 0 9', solution: ['Nxd5', 'Nxd5', 'Nxd5', 'Qxd5', 'Bb5', 'Qxb5', 'axb5'] },
-  { fen: 'r1bq1rk1/2p2ppp/p1pb1n2/1p2p3/4P3/1BP2N2/PP1P1PPP/RNBQR1K1 w - - 0 9', solution: ['Nxe5', 'Bxe5', 'Rxe5', 'Nxe4', 'Bxe4', 'd5', 'Bxd5+', 'cxd5', 'Rxd5'] },
-  { fen: '4r1k1/pp3ppp/1q2pn2/3pN3/3P4/2P5/PP3PPP/R2QR1K1 w - - 0 1', solution: ['Nxf7', 'Rxe8+', 'Qxe8', 'Nxd8+', 'Kh8', 'Nxb7'] },
-  { fen: 'r2q1rk1/1b1nbppp/p2ppn2/1p4B1/3NPP2/2N5/PPPQ2PP/2KR1B1R w - - 0 1', solution: ['Nxd6', 'Nxd6', 'e5', 'Ne4', 'Bxd7', 'Qxd7', 'Nxf6+'] },
-  { fen: '4r1k1/5ppp/p7/1p6/3B4/1P6/P4PPP/4R1K1 w - - 0 1', solution: ['Be5', 'f6', 'Bxf6', 'Rxe1+', 'Rxe1'] },
-  { fen: 'rn3rk1/pp2bppp/2p1pn2/q7/2PP4/2N1PN2/PPQ2PPP/R1B1KB1R w KQ - 0 9', solution: ['Ne4', 'Nxe4', 'Qxe4', 'Nbd7', 'd5', 'cxd5', 'Qb4'] },
-  { fen: '1k5r/pp3p1p/2p3p1/4p3/4P3/2P3P1/PP3P1P/1K5R w - - 0 1', solution: ['Rxh7', 'Rxh7', 'Rxh7'] },
-  { fen: 'r2qkb1r/ppp2ppp/2n1pn2/3p1b2/2PP4/2N1PN2/PP3PPP/R1BQKB1R w KQkq - 0 6', solution: ['Qb3', 'Bxc1', 'Rxc1', 'Bb4', 'cxd5', 'Nxd5', 'Nxd5', 'Qxd5', 'Bc4'] },
-  { fen: '6k1/pp3ppp/2p5/3p4/3P4/2P5/PP3PPP/6K1 w - - 0 1', solution: ['Kf1', 'Kf8', 'Ke2', 'Ke7', 'Kd3', 'Kd6', 'Kc4'] },
-  { fen: 'r1b1kb1r/ppp2ppp/2n5/3qp3/4P3/2NB4/PPP2PPP/R1BQK2R w KQkq - 0 1', solution: ['Nxe5', 'Nxe5', 'Bxh7+', 'g6', 'Qh5'] },
-  { fen: 'r1bqr1k1/pp1n1ppp/2pb1n2/3pp3/3PP3/2N1BN2/PPP1BPPP/R2QK2R w KQ - 0 9', solution: ['dxe5', 'dxe5', 'Nxe5', 'Nxe5', 'Bxb5', 'Qd7', 'Bxd7', 'Nxd7'] },
-  { fen: 'r3k2r/pp3ppp/2p5/3pq3/8/2N5/PPP2PPP/R2QR1K1 b kq - 0 1', solution: ['Qxe1+', 'Rxe1+', 'Kd7'] },
-  { fen: 'r1bq1rk1/ppp1bppp/2n1pn2/3p4/3P1B2/2NBPN2/PPP2PPP/R2QK2R w KQ - 0 8', solution: ['Bxh7+', 'Nxh7', 'Qd3', 'g6', 'Bh6', 'Kg7', 'Bg5'] },
-  { fen: '5k2/5pp1/7p/5P2/8/8/5PPP/5K2 w - - 0 1', solution: ['fxg6', 'fxg6', 'f4', 'Ke7', 'f5', 'gxf5', 'g4'] },
+  {
+    fen: "rnbqkbnr/pppp3p/5p2/6p1/4Pp1P/5N2/PPPP2P1/RNBQKB1R w - - 0 1",
+    solution: ["Nxg5", "fxg5", "Qh5+", "Ke7", "Qxg5+", "Ke8", "Qh5+", "Ke7", "Qe5+"]
+  },
+  {
+    fen: "rnbqkb1r/pp1p2pp/2p2p2/4p3/2B5/2P2N2/PPP2PPP/R1BQ1RK1 w - - 0 1",
+    solution: ["Nxe5", "d5", "Qh5+", "g6", "Nxg6", "hxg6", "Qxh8"]
+  },
+  {
+    fen: "2r3k1/p3qppp/2pr4/Q2b4/1P2p3/4P3/P3BPPP/2RR2K1 w - - 0 1",
+    solution: ["Rxd5", "Rxd5", "Qxd5"]
+  },
+  {
+    fen: "6k1/2p3pp/q3pn2/1pp1p3/4P3/1P1P1P2/rNP2P1P/1Q3RK1 w - - 0 1",
+    solution: ["Na4", "Ra3", "Qb2", "b4", "Qxe5"]
+  },
+  {
+    fen: "8/1p3q1k/2p3pp/4P1r1/8/4Q3/PP5P/3R3K b - - 0 1",
+    solution: ["Rxe5", "Qxe5", "Qf3+", "Kg1", "Qxd1+"]
+  },
+  {
+    fen: "r5k1/1b1n2r1/p3n2q/1p1pPRN1/2pP3P/2P3P1/PPBQ4/5R1K w - - 0 1",
+    solution: ["Rf6", "Nxf6", "Rxf6"]
+  },
+  {
+    fen: "r1b2rk1/p2p1p2/2p5/1p2PPqn/1b1p2N1/1B1P3Q/PPP3PP/R4RK1 w - - 0 1",
+    solution: ["Qxh5", "Qxh5", "Nf6+", "Kh8", "Nxh5"]
+  },
+  {
+    fen: "r5r1/p1p1k3/3q3B/5p2/4p3/1P6/P1P1QPP1/R4RK1 b - - 0 1",
+    solution: ["Rxg2+", "Kxg2", "Rg8+", "Kh1", "Qxh6+", "Qh5", "Qxh5#"]
+  },
+  {
+    fen: "6rk/p1q2p2/2p1rb1P/1p2pN2/4P1Q1/2PP4/PPB5/2K4R w - - 0 1",
+    solution: ["Qg7+", "Rxg7", "hxg7+", "Kg8", "Rh8#"]
+  },
+  {
+    fen: "4q1k1/2r3pp/1p6/8/1b2N3/4R1P1/PP3P1P/R5K1 w - - 0 1",
+    solution: ["Nf6+", "gxf6", "Rxe8+"]
+  },
+  {
+    fen: "4k3/1bp4r/p7/1p1P4/2P3pN/1P2r1P1/1BP2RPK/8 b - - 0 1",
+    solution: ["Rxh4+", "gxh4", "g3+", "Kg1", "gxf2+"]
+  },
+  {
+    fen: "2q4k/5Qp1/4B2p/p1p5/1P6/6PK/r4P1P/8 b - - 0 1",
+    solution: ["Rxf2", "Qxf2", "Qxe6+"]
+  },
+  {
+    fen: "5Rnk/pp1q4/7p/3p2rN/3Pp1Q1/2P5/PP5P/6K1 w - - 0 1",
+    solution: ["Qxg5", "hxg5", "Rxg8+", "Kxg8", "Nf6+", "Kf7", "Nxd7"]
+  },
+  {
+    fen: "r1bq2k1/pp3rpp/2n2b2/3p1p2/3P4/BQPB1N2/P4PPP/R3R1K1 w - - 0 1",
+    solution: ["Qxd5", "Qxd5", "Re8+", "Rf8", "Rxf8#"]
+  },
+  {
+    fen: "2r3k1/pb2bp1p/1p2p1p1/8/q1NPP3/3B4/P3QPPP/3R2K1 b - - 0 1",
+    solution: ["Bxe4", "Bxe4", "Rxc4"]
+  },
+  {
+    fen: "6r1/2r1k3/R3p3/p4pPp/1pPK1P2/1P3B1P/P7/8 w - - 0 1",
+    solution: ["Rxe6+", "Kxe6", "Bd5+", "Kd6", "Bxg8"]
+  },
+  {
+    fen: "r4r1k/pppqNppp/3p1B2/4p3/3nP3/3P1b2/PPPQ1PPP/R4RK1 w - - 0 1",
+    solution: ["Bxg7+", "Kxg7", "Qg5+", "Kh8", "Qf6#"]
+  },
+  {
+    fen: "3r2k1/2p2pp1/p1Q2n1p/7q/8/1P1N2P1/P1P2P2/R3K3 b - - 0 1",
+    solution: ["Rxd3", "cxd3", "Qe5+", "Kd2", "Qxa1"]
+  },
+  {
+    fen: "r1b2rk1/pp3qpp/2p1p3/2Ppb1PP/5B2/3BP3/PP3Q2/2R1K2R w - - 0 1",
+    solution: ["Bxh7+", "Kxh7", "g6+"]
+  },
+  {
+    fen: "2r2rk1/pQ1n1pp1/1p2p2p/3p4/P2P4/4P2P/1qB2PP1/2R2RK1 w - - 0 1",
+    solution: ["Bh7+", "Kxh7", "Rxc8", "Rxc8", "Qxc8"]
+  }
 ]
 
-const PUZZLES_BLOCK_3 = PUZZLES_BLOCK_1.map(p => ({ fen: p.fen, solution: [...p.solution] }))
-const PUZZLES_BLOCK_4 = PUZZLES_BLOCK_2.map(p => ({ fen: p.fen, solution: [...p.solution] }))
-
+const PUZZLES_BLOCK_3 = [
+  {
+    fen: "3k4/p1p2prr/1p5N/3PRPP1/b1P5/4B3/P4K2/8 w - - 0 1",
+    solution: ["f6", "Rg6"]
+  },
+  {
+    fen: "r3k2r/pbp2qb1/1pn1p2p/3nP1pQ/3PNp2/2PB4/PP1N1BPP/R4RK1 w - - 0 1",
+    solution: ["Nd6+", "cxd6", "Bg6"]
+  },
+  {
+    fen: "4k3/1r2r1pp/1nR2p2/pp1p4/1N1P2P1/1R2PP2/PP3K1P/8 w - - 0 1",
+    solution: ["Rxb6", "axb4", "Rxb7", "Rxb7", "Rxb4"]
+  },
+  {
+    fen: "7r/1p3pp1/pn1kb2p/3p4/3N1P1P/PP1BP3/3K2P1/2R5 w - - 0 1",
+    solution: ["Bxa6", "bxa6", "Rc6+", "Ke7", "Rxb6"]
+  },
+  {
+    fen: "r3nrk1/1bp2ppp/pp2p3/3q2N1/1b1PNP2/3B2P1/PP2QP1P/2RR2K1 w - - 0 1",
+    solution: ["Nxh7"]
+  },
+  {
+    fen: "r1b1k2r/2qp1ppp/ppnbpn2/8/2PNP3/P1N1BP2/1P4PP/2RQKB1R w - - 0 1",
+    solution: ["Ndb5", "axb5", "Nxb5", "Bg3+", "hxg3", "Qxg3+", "Bf2"]
+  },
+  {
+    fen: "4r1k1/6qp/pp4p1/2pP4/4Pp2/1P6/P1R3PP/4Q2K b - - 0 1",
+    solution: ["Rxe4", "Qxe4", "Qa1+"]
+  },
+  {
+    fen: "2bq1r2/4bpk1/4pp1N/7Q/1p1p4/3B4/PP3PPP/R5K1 w - - 0 1",
+    solution: ["Nxf7", "Rxf7", "Qh7+", "Kf8", "Qh8#"]
+  },
+  {
+    fen: "r3r1k1/3b2pp/2p5/p1RpPp2/3Q1P2/1q2P1P1/6BP/R5K1 w - - 0 1",
+    solution: ["Rxd5", "Be6", "Rd6"]
+  },
+  {
+    fen: "r3br1k/pp5p/4B1p1/4NpP1/P2Pn3/q1PQ3R/7P/3R2K1 w - - 0 1",
+    solution: ["Rxh7+", "Kxh7", "Qh3+", "Kg7", "Qh6#"]
+  },
+  {
+    fen: "5rk1/ppp2ppp/6q1/2b1P3/3r4/2N1BQ1b/PP3PPP/R3R1K1 b - - 0 1",
+    solution: ["Bxg2", "Qg3", "Rg4", "Bxc5", "Rxg3"]
+  },
+  {
+    fen: "4r2k/ppp3pp/8/1PPb1p2/3P1P1b/P1Q2p1P/7R/R4KBq b - - 0 1",
+    solution: ["Qg2+", "Rxg2", "fxg2#"]
+  },
+  {
+    fen: "3r1b1k/pp4p1/2p1Qp2/5N2/PP2Pp2/2Pq4/5PKP/5R2 b - - 0 1",
+    solution: ["f3+", "Kg1", "Qxf1+", "Kxf1", "Rd1#"]
+  },
+  {
+    fen: "r1b2rk1/pp4pp/2pb4/3p1pq1/2PP4/1N1BPR2/PPQ3PP/4R1K1 b - - 0 1",
+    solution: ["Bxh2+", "Kxh2", "Qh4+", "Rh3", "Qxe1"]
+  },
+  {
+    fen: "q6r/1b4bp/4k1p1/1p2Pn2/2pPp1Q1/2P5/1P1N2PP/2B2RK1 w - - 0 1",
+    solution: ["Rxf5", "gxf5", "Qxg7"]
+  },
+  {
+    fen: "5rk1/1b1p1ppp/1qr1p3/p2pP3/P4P2/Q2B4/1PP3PP/R4R1K w - - 0 1",
+    solution: ["Bxh7+", "Kxh7", "Qxf8"]
+  },
+  {
+    fen: "5r1k/pp4pp/2p5/8/4n3/5NPQ/P3Bq1P/4R2K b - - 0 1",
+    solution: ["Qxe1+", "Nxe1", "Nf2+", "Kg2", "Nxh3", "Nf3", "Nf4+", "gxf4", "Rxf4"]
+  },
+  {
+    fen: "r4rk1/pp2bppp/1qp1p3/4Pb2/Q1P1nB2/2N5/PP1RBPPP/5RK1 w - - 0 1",
+    solution: ["Nxe4", "Bxe4", "c5", "Bxc5", "Qxe4"]
+  },
+  {
+    fen: "2r5/pp1bkp1Q/2nbpq2/3p1p2/3P1Pr1/2NBP1N1/PP4PP/2R2RK1 w - - 0 1",
+    solution: ["Nxf5+", "exf5", "Nxd5+", "Ke6", "Nxf6"]
+  },
+  {
+    fen: "r1bqr1k1/1p1nbpp1/p1p3p1/3p4/3P1B2/2NBP2P/PP3PP1/2RQ1RK1 w - - 0 1",
+    solution: ["Nxd5", "cxd5", "Bc7"]
+  }
+]
+const PUZZLES_BLOCK_4 = [
+  {
+    fen: "3r2k1/q1p1nppp/p3n3/1pb1p3/4P2N/1PP3PP/PBB1QPK1/7R b - - 0 1",
+    solution: ["Bxf2", "Qxf2", "Qxf2+", "Kxf2", "Rd2+", "Kg1", "Rxc2"]
+  },
+  {
+    fen: "1Q6/p4pkp/3p2p1/3P4/q7/P3rBbP/6P1/5R1K b - - 0 1",
+    solution: ["Rxf3", "gxf3", "Qc2", "Rf2", "Qxf2", "Qb2+", "Qxb2"]
+  },
+  {
+    fen: "r1bqk2r/p1pn1pp1/1p2pn1p/8/3P4/B1PB4/P1P1QPPP/R3K1NR w - - 0 1",
+    solution: ["Qxe6+", "fxe6", "Bg6#"]
+  },
+  {
+    fen: "r2qk2r/1p1b1pp1/p1pBpn1p/2P1N3/1n1P4/3B4/PPQ2PPP/2KR3R w - - 0 1",
+    solution: ["Bg6", "Qa5", "Bxf7+"]
+  },
+  {
+    fen: "1r2r1k1/p1pbqppp/Q2b1n2/3p4/P2P4/2P5/1P2BPPP/R1B1KN1R b - - 0 1",
+    solution: ["Bb5", "Qxb5", "Rxb5"]
+  },
+  {
+    fen: "1r4k1/pqp2pbp/2Q2np1/1N2p3/8/1P5P/PBP2PP1/3R2K1 w - - 0 1",
+    solution: ["Rd8+", "Rxd8", "Qxb7"]
+  },
+  {
+    fen: "rn1qkb1r/pp3p1b/2p1pnpp/4N3/2B4P/6N1/PPPPQPP1/R1B1K2R w KQkq - 0 1",
+    solution: ["Nxf7", "Qe7", "Nxh8"]
+  },
+  {
+    fen: "3n4/2prR1pk/p2r1p1p/1p5P/5P1P/P1B2K2/1PP5/4R3 w - - 0 1",
+    solution: ["Bxf6", "Rxe7", "Bxe7"]
+  },
+  {
+    fen: "6r1/1p2R3/p5k1/2p5/4Nr1P/8/PP5P/6K1 b - - 0 1",
+    solution: ["Rxe4", "Rxe4", "Kf5+", "Kf2", "Kxe4"]
+  },
+  {
+    fen: "r1b2rk1/ppqnbppp/2p1pn2/3p2B1/2PP4/2NBPN2/PPQ2PPP/R3K2R w - - 0 1",
+    solution: ["Bxh7+", "Nxh7", "Bxe7"]
+  },
+  {
+    fen: "r1b1qrk1/pppp1ppp/1bn3n1/3Np1BQ/2B1P3/3P1N2/PPP2PPP/R3K2R w - - 0 1",
+    solution: ["Nf6+", "gxf6", "Bxf6", "Bxf2+", "Kxf2"]
+  },
+  {
+    fen: "r2qrbk1/1bp2ppp/p2p1n2/2p2NB1/4P3/2N2Q2/PPP2PPP/R3R1K1 w - - 0 1",
+    solution: ["Nh6+", "gxh6", "Bxf6"]
+  },
+  {
+    fen: "r4r1k/ppn1NBpp/4b3/4P3/3p1R2/1P6/P1P3PP/R5K1 w - - 0 1",
+    solution: ["Ng6+", "hxg6", "Rh4#"]
+  },
+  {
+    fen: "2r2rk1/pp1bnp2/3q1n1Q/3p1P2/4p2N/1BPP4/P1P3PP/R4RK1 b - - 0 1",
+    solution: ["Qxh2+", "Kxh2", "Ng4+", "Kg3", "Nxh6"]
+  },
+  {
+    fen: "5rk1/pbp2ppp/qr6/8/5Q2/1PP5/P4PP1/R1B2RK1 b - - 0 1",
+    solution: ["Bxg2", "Kxg2", "Rg6+"]
+  },
+  {
+    fen: "r1b2rk1/pp1p1ppp/2n2n2/q7/2P5/P1N2NP1/3QPKBP/R1B4R b - - 0 1",
+    solution: ["Qxc3", "Qxc3", "Ne4+", "Kf1", "Nxc3"]
+  },
+  {
+    fen: "3Q4/p4pkp/1p3np1/2q5/4p3/4P1N1/PP3PPP/6K1 w - - 0 1",
+    solution: ["Qxf6+", "Kxf6", "Nxe4+", "Ke5", "Nxc5", "bxc5", "Kf1"]
+  },
+  {
+    fen: "3r4/p2q1pkp/1pn1bnp1/2p1p3/P1N1P3/1PP1Q1PP/5PK1/4RBN1 b - - 0 1",
+    solution: ["Nxe4", "Qxe4", "Bd5"]
+  },
+  {
+    fen: "2kr4/p1p2ppp/3rb3/8/2P5/1R1BR3/P4PPP/5K2 b - - 0 1",
+    solution: ["Rxd3", "Rexd3", "Rxd3", "Rxd3", "Bxc4"]
+  },
+  {
+    fen: "6k1/3q3p/p1p3pQ/1p1p4/3P2RP/1P3P2/r3r1P1/5R1K b - - 0 1",
+    solution: ["Rxg2", "Rxg2", "Qh3+"]
+  }
+]
 const SEED_BLOCKS = [
   { name: 'Bloque 1', description: 'Puzzles 1–20', puzzles: PUZZLES_BLOCK_1 },
   { name: 'Bloque 2', description: 'Puzzles 21–40', puzzles: PUZZLES_BLOCK_2 },
